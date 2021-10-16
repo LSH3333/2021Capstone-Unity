d10e56413e9a515c049cbf00cc24427012279625
LSH <dltpgustpgus@gmail.com>
2021-10-16 16:31:21 +0900

    Unity->Text 파일 생성 기능 테스트 완료, 로고씬, Fade out 기능 완료

---
+++ Assets/Scripts/Fade.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Fade : MonoBehaviour
+{
+    private int _nextScene = 0;
+
+
+    public void endFadeIn()
+    {
+        gameObject.SetActive(false);
+    }
+
+    public void endFadeOut()
+    {
+        SceneManager.LoadScene(_nextScene);
+    }
+
+    public void setFadeOut()
+    {
+        GetComponent<Animator>().SetTrigger("SetFadeout");
+    }
+
+    public void setNextScene(int _idx)
+    {
+        _nextScene = _idx;
+    }
+}
---
+++ Assets/Scripts/InputToText.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.IO;
+
+public class InputToText : MonoBehaviour
+{    
+    private GameObject[] inputFieldsObj;
+
+    private void Awake()
+    {
+        // create folder 
+        Directory.CreateDirectory(Application.streamingAssetsPath + "/Created_Text/");
+
+        // Find all Input Fields gameobject 
+        inputFieldsObj = GameObject.FindGameObjectsWithTag("InputField");
+    }
+
+    // 버튼 누르면 모든 input field들에 있는 text에 따라 텍스트파일 생성하도록 
+    public void CreateTextFile()
+    {
+        // 만들 txt file의 이름 
+        string txtDocumentName = Application.streamingAssetsPath + "/Created_Text/" + "TextOutput" + ".txt";
+
+        // 시작 전 text 파일 초기화         
+        File.WriteAllText(txtDocumentName, "");
+
+        foreach (var x in inputFieldsObj)
+        {            
+            CreateText(x, txtDocumentName);            
+        }
+    }
+
+    public void CreateText(GameObject obj, string txtDocumentName)
+    {
+        InputField _input = obj.GetComponent<InputField>();
+        if(_input.text == "") _input.text = "THIS FIELD IS EMPTY"; // 비어있다면 
+        // txt파일에 input field에 있는 문자열들 추가 (개행 포함) 
+        File.AppendAllText(txtDocumentName, _input.text + "\n");
+    }
+
+}
