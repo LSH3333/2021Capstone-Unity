8290b242b668e3fb9bbc4a58209b57580f17d47d
LSH3333 <dltpgustpgus@gmail.com>
2021-11-16 13:32:26 +0900

    button hovering, sound

--- Assets/Scripts/AreYouSureBoxButtonManager.cs
+++ Assets/Scripts/AreYouSureBoxButtonManager.cs
@@ -3,17 +3,41 @@ using UnityEngine.UI;
 
 public class AreYouSureBoxButtonManager : MonoBehaviour
 {
+    public AudioSource onSound;
+
 
     public void OnClickYes()
     {
         // 모든 씬에는 ManageScene 게임오브젝트가 있고 그 내부에 씬을 컨트롤하는 클래스가 있고 ,
         // 해당 클래스안에는 DoBeforeMovingToNextScene 함수가 존재한다 
-        GameObject.Find("ManageScene").SendMessage("DoBeforeMovingToNextScene");        
+        GameObject.Find("ManageScene").SendMessage("DoBeforeMovingToNextScene");
+        ClickSnd();
+
     }
 
     public void OnClickNo()
-    {
-        Destroy(GameObject.Find("AreYouSureBox(Clone)"));
+    {                
         GameObject.Find("NextBtn").GetComponent<Button>().interactable = true;
+
+        // click sound
+        ClickSnd();
+
+        Destroy(gameObject);
     }
+
+    public void MouseEnterButton()
+    {
+        onSound.Play();
+    }
+
+    private void ClickSnd()
+    {
+        // click sound 
+        GameObject clickSnd = Resources.Load("clickSnd") as GameObject;
+        GameObject spawnedSnd = Instantiate(clickSnd, Vector2.zero, Quaternion.identity);
+        spawnedSnd.GetComponent<AudioSource>().Play();
+        Destroy(spawnedSnd, 1f);
+    }
+
+    
 }
--- Assets/Scripts/ButtonManager.cs
+++ Assets/Scripts/ButtonManager.cs
@@ -1,12 +1,32 @@
 ﻿using UnityEngine;
+using UnityEngine.UI;
 
 public class ButtonManager : MonoBehaviour
 {
     public AudioSource buttonClickSound;
+    public AudioSource onSound;
 
     // Button Click 시 사운드 
     public void OnClickButtonSound()
     {
         buttonClickSound.Play();
+    }    
+
+    // 버튼에 마우스 올라갈 시 이벤트 트리거 
+    public void MouseOnButton()
+    {
+        // hovering 
+        ((RectTransform)gameObject.transform).sizeDelta = new Vector2(300f, 150f);
+        ((RectTransform)gameObject.transform.GetChild(0).transform).sizeDelta = new Vector2(300f, 150f);
+        gameObject.transform.GetChild(1).GetComponent<Text>().fontSize = 50;
+        onSound.Play();
+    }
+
+    // 버튼에서 마우스 땔 시 
+    public void MouseExitButton()
+    {
+        ((RectTransform)gameObject.transform).sizeDelta = new Vector2(250f, 100f);
+        ((RectTransform)gameObject.transform.GetChild(0).transform).sizeDelta = new Vector2(250f, 100f);
+        gameObject.transform.GetChild(1).GetComponent<Text>().fontSize = 40;
     }
 }
